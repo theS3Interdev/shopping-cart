388f31183040c315442b743a666bce4f1dd4468c
Michael Owuor <michael.owuor@gmail.com>
2023-09-06 08:18:22 +0300

    feat: save items to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import data from './data';
 
@@ -25,11 +25,21 @@ function App() {
 		const exist = cartItems.find((x) => x.id === product.id);
 
 		if (exist) {
-			setCartItems(
-				cartItems.map((x) => (x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x))
+			const newCartItems = cartItems.map((x) =>
+				x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
 			);
+
+			setCartItems(newCartItems);
+
+			/* save to local storage */
+			localStorage.setItem('cartItems', JSON.stringify(newCartItems));
 		} else {
-			setCartItems([...cartItems, { ...product, qty: 1 }]);
+			const newCartItems = [...cartItems, { ...product, qty: 1 }];
+
+			setCartItems(newCartItems);
+
+			/* save to local storage */
+			localStorage.setItem('cartItems', JSON.stringify(newCartItems));
 		}
 	};
 
@@ -39,13 +49,29 @@ function App() {
 		if (!exist) return;
 
 		if (exist.qty === 1) {
-			setCartItems(cartItems.filter((x) => x.id !== product.id));
+			const newCartItems = cartItems.filter((x) => x.id !== product.id);
+
+			setCartItems(newCartItems);
+
+			/* save to local storage */
+			localStorage.setItem('cartItems', JSON.stringify(newCartItems));
 		} else {
-			setCartItems(
-				cartItems.map((x) => (x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x))
+			const newCartItems = cartItems.map((x) =>
+				x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
 			);
+
+			setCartItems(newCartItems);
+
+			/* save to local storage */
+			localStorage.setItem('cartItems', JSON.stringify(newCartItems));
 		}
 	};
+
+	useEffect(() => {
+		const items: string | null = localStorage.getItem('cartItems');
+
+		setCartItems(items ? JSON.parse(items) : []);
+	}, []);
 
 	return (
 		<div>
